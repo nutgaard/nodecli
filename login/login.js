a37f7e8a0527a6d822381866955a67067d68df92
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-10-05 13:35:23 +0200

    [feat][-] Spor etter hvilke noder man skal apne

--- login/login.js
+++ login/login.js
@@ -1,11 +1,15 @@
-const execa = require('execa');
+const cp = require('child_process');
 const logging = require('./../utils/logging');
+const inquirer = require('inquirer');
 const Fetcher = require('./../utils/fetch');
 
 function loginInto(node) {
     getCredentialsFromFasit(node)
         .then((password) => {
-            execa.shellSync(`putty -ssh deployer@${node} -pw ${password}`);
+            // Some magic to create a fully-standalone non-blocking process
+            let cmd = `-ssh deployer@${node} -pw ${password}`.split(' ');
+            const subprocess = cp.spawn('putty', cmd, { detached: true, stdio: 'ignore' });
+            subprocess.unref();
         });
 }
 
@@ -30,16 +34,18 @@ module.exports = function(environment, application, num) {
     } else {
         getHostname(environment, application)
             .then((nodes) => {
-
                 if (nodes.length === 1) {
                     loginInto(nodes[0]);
                 } else if (num) {
                     loginInto(nodes[num - 1]);
                 } else {
                     logging.info('There are several nodes for %s in %s. Please specify which one you want to log into', application, environment);
-                    nodes.forEach((node, i) => {
-                        logging.info('%d: %s', (i + 1), node)
-                    });
+                    inquirer.prompt([{
+                        type: 'checkbox',
+                        name: 'nodes',
+                        message: `Hvilke noder vil du logge inn på?`,
+                        choices: nodes
+                    }]).then((answer) => answer.nodes.forEach(loginInto));
                 }
             });
     }
