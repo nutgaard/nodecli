6ad8aab6d05c940a892d9476a3d877bf5aaa9e5f
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-10-24 12:03:32 +0200

    [feat][-] La til `gf` og `gr`
    Henholdsvis `git fetch` og `git rebase`

--- doskey/doskey.cmd
+++ doskey/doskey.cmd
@@ -3,6 +3,8 @@
 DOSKEY gs=git status
 DOSKEY gl=git pull $*
 DOSKEY gp=git push $*
+DOSKEY gf=git fetch $*
+DOSKEY gr=git rebase $*
 DOSKEY lg=git lg
 DOSKEY glg=git glg
 DOSKEY mci=mvn clean install $*
