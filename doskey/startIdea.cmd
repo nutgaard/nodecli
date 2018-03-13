f31114bd6e6cddcff173fc403cba49ac3cef3c5f
Nicklas Utgaard <nutgaard@gmail.com>
2018-03-13 22:11:49 +0100

    [feat][-] La `idea` kommando for å åpne en mappe i idea
    Krever at `IDEA_HOME` er satt som miljøvariabel

---
+++ doskey/startIdea.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+REM "%IDEA_HOME%\idea64.exe" "%cd%"
+SETLOCAL
+if "%1" == "" (
+    SET openpath=%cd%
+) else (
+    SET openpath=%cd%\%1%
+)
+
+start /b "[IDEA]" "%IDEA_HOME%\idea64.exe" "%openpath%" &
+ENDLOCAL
