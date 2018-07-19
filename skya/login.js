5b59965fe65cc02fbecb622d1398be98983f5601
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-07-19 13:11:40 +0200

    [KAIZEN-0] Fixed skya login script

--- skya/login.js
+++ skya/login.js
@@ -21,7 +21,11 @@ function getCredentialsFromFasit(node) {
 
 function getHostname(environment, application) {
     return Fetcher.fetchJson(`https://fasit.adeo.no/api/v2/applicationinstances?environment=${environment}&application=${application}&usage=true`)
-        .then((resp) => resp.sort((a, b) => new Levenshtein(a.application, b.application).distance))
+        .then((resp) => resp.sort((a, b) => {
+            const d1 = new Levenshtein(application, a.application).distance;
+            const d2 = new Levenshtein(application, b.application).distance;
+            return d1 - d2;
+        }))
         .then((resp) => resp[0].cluster.ref)
         .then((clusterRef) => Fetcher.fetchJson(clusterRef))
         .then((cluster) => cluster.nodes.map((node) => node.name))
